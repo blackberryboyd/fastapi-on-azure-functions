4f573571d58b75ba21e2fed34d336a72c513054f
blackberryboyd <boyd30@gmail.com>
2026-06-16 17:54:20 -0600

    add gentenv

--- database.py
+++ database.py
@@ -1,8 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import dotenv
+import os
 
-db_test_pw = dotenv.get_key(".env", "azure_test_sql_pw")
+db_test_pw = os.getenv("AZURE_SQL_PASSWORD")
 # Replace with your Azure SQL Server connection string
 SQLALCHEMY_DATABASE_URL = "mssql+pyodbc://office-workout.database.windows.net:1433/database=free-sql-db-9896707;adminuser;{db_test_pw};;Encrypt=yes;TrustServerCertificate=no;Driver={ODBC Driver 17 for SQL Server};"
 
