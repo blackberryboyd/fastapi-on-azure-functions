6047917dfc7e18fd756b13b174e0dc635bd90bdd
blackberryboyd <boyd30@gmail.com>
2026-06-16 17:34:20 -0600

    change db url

--- database.py
+++ database.py
@@ -1,8 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+import dotenv
 
+db_test_pw = dotenv.get_key(".env", "azure_test_sql_pw")
 # Replace with your Azure SQL Server connection string
-SQLALCHEMY_DATABASE_URL = "mssql+pyodbc://sql-server1-off-ice.database.windows.net:1433/database=db1;Driver={ODBC Driver 17 for SQL Server};"
+SQLALCHEMY_DATABASE_URL = "mssql+pyodbc://office-workout.database.windows.net:1433/database=free-sql-db-9896707;adminuser;{db_test_pw};;Encrypt=yes;TrustServerCertificate=no;Driver={ODBC Driver 17 for SQL Server};"
+
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
